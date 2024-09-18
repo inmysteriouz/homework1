94bddd997e1068f343d75027ad31d638f3c80698
Maksim Aksenov <461520933@mail.ru>
2024-09-17 20:10:00 -0700

    completed last homework

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+get_mask_card_number('1234567890123456')
+print(__name__)
---
+++ src/masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    return f"**{account_number[-4:]}"
+
+
+if __name__ == '__main__':
+
+    card_number_str = '1234567890123456'
+    account_num = '12345678901234561234'
+
+    print(get_mask_card_number(card_number_str))
+    print(get_mask_account(account_num))
